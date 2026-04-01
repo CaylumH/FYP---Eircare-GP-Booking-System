fbd93d22472b11e27010c33e289508ae9dc22971
caylumhurley <caylumhurley@gmail.com>
2026-04-01 02:54:52 +0100

    initial eircare project setup ready for deployment and testing

---
+++ eircare-backend/src/main/java/com/example/eircare_backend/EircareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.eircare_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EircareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EircareBackendApplication.class, args);
+	}
+
+}
---
+++ eircare-backend/src/test/java/com/example/eircare_backend/EircareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eircare_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EircareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
